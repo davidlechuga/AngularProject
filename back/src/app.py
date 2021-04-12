3866d6d8beb57f5d61fc1d29d85bd314789d3574
davidlechuga <lechugazteca@hotmail.com>
2021-04-12 15:03:20 -0500

    back cisco

--- back/src/app.py
+++ back/src/app.py
@@ -6,6 +6,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from bson import json_util
 from bson.objectid import ObjectId
 
+from netmiko import ConnectHandler
+from getpass import getpass
+# import paramiko
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -14,7 +18,87 @@ app.config['MONGO_URI'] = 'mongodb+srv://David:1Diosasegu@cooperativa.ne6tg.mong
 mongo = PyMongo(app)
 
 
-@app.route('/users', methods=['POST'])
+## init config cisco ##
+
+# devices = [{
+#     "device_type": "cisco_xr",
+#     "ip": "sbx-iosxr-mgmt.cisco.com",
+#     "username": "***",
+#     "password": "***",
+#     "port": "8181",
+# }, {
+#     "device_type": "cisco_xe",
+#     "ip": "ios-xe-mgmt-latest.cisco.com",
+#     "username": "***",
+#     "password": "***",
+#     "port": "8181",
+# }]
+
+
+cisco_881 = {
+    'device_type': 'cisco_ios',
+    'host':   '200.4.144.3',
+    'username': 'cisco',
+    'password': 'cisco',
+    'port': 50051,          # optional, defaults to 22
+    'secret': 'secret',     # optional, defaults to ''
+}
+
+# net_connect = ConnectHandler(**cisco_881)
+# net_connect.enable()
+
+for device in (cisco_881):
+    net_connect = ConnectHandler(**cisco_881)
+
+    # output = net_connect.send_command("show ip interface brief")
+    output = net_connect.send_command('show config')
+    net_connect.disconnect()
+    print("-"*100)
+    print(output)
+    print("-"*100)
+
+
+# sshConfirm = net_connect.find_prompt()
+# print('successfully logged in ' + sshConfirm)
+
+# end config cisco
+
+# 1 -. Time to ejecute Excersise  "show arp"
+
+# all_devices = [cisco_881]
+
+# start_time = datetime.now()
+# for a_device in all_devices:
+#     net_connect = ConnectHandler(**a_device)
+#     output = net_connect.send_command("show arp")
+#     print(f"\n\n--------- Device {a_device['device_type']} ---------")
+#     print(output)
+#     print("--------- End ---------")
+
+#     end_time = datetime.now()
+# total_time = end_time - start_time
+
+###  2-. show ip brief ####
+
+# output = net_connect.send_command('show ip int brief')
+# print(output)
+
+
+# output = net_connect.send_command('show interfaces GigabitEthernet1')
+# print(output)
+
+
+###  3-. loging show  ####
+
+# output = net_connect.send_command('show run | inc logging')
+# print(output)
+# config_commands = ['logging buffered 19999']
+
+# output = net_connect.send_command("show ip cache flow")
+# print(output)
+
+
+@ app.route('/users', methods=['POST'])
 def create_user():
     username = request.json['username']
     password = request.json['password']
@@ -39,28 +123,28 @@ def create_user():
     return {'message': 'received'}
 
 
-@app.route('/users', methods=['GET'])
+@ app.route('/users', methods=['GET'])
 def get_users():
     users = mongo.db.users.find()
     response = json_util.dumps(users)
     return Response(response, mimetype='application/json')
 
 
-@app.route('/users/<id>', methods=['GET'])
+@ app.route('/users/<id>', methods=['GET'])
 def get_user(id):
     user = mongo.db.users.find_one({'_id': ObjectId(id)})
     response = json_util.dumps(user)
     return Response(response, mimetype="application/json")
 
 
-@app.route('/users/<id>', methods=['DELETE'])
+@ app.route('/users/<id>', methods=['DELETE'])
 def delete_user(id):
     mongo.db.users.delete_one({'_id': ObjectId(id)})
     response = jsonify({'message': 'User' + id + 'was deleted successfully'})
     return response
 
 
-@app.route('/users/<id>', methods=['PUT'])
+@ app.route('/users/<id>', methods=['PUT'])
 def update_user(id):
     username = request.json['username']
     email = request.json['email']
@@ -78,7 +162,7 @@ def update_user(id):
         return response
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def not_found(error=None):
     response = jsonify({
         'message': 'Resource Not Found:' + request.url,
