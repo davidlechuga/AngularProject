22c4fbbe66412a479626dd755228d260e84ec14a
davidlechuga <47370612+davidlechuga@users.noreply.github.com>
2021-04-14 13:03:59 -0500

    Update app.py

--- back/src/app.py
+++ back/src/app.py
@@ -14,7 +14,7 @@ from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 
-app.config['MONGO_URI'] = 'mongodb+srv://David:1Diosasegu@cooperativa.ne6tg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = ''
 mongo = PyMongo(app)
 
 
